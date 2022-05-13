96088788d45ca728fc794504e616057369bab58e
Thallesp <contato@thalles.me>
2022-05-12 22:56:28 -0300

    :bug: Register usecases and controller in the module

--- src/modules/repositories/RepositoryModule.ts
+++ src/modules/repositories/RepositoryModule.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
-import { NewRepositoryController } from './useCases/newRepository/NewRepositoryController';
-import { NewRepositoryUseCase } from './useCases/newRepository/NewRepositoryUseCase';
 import { IDokkuProvider } from '../../shared/providers/DokkuProvider/IDokkuProvider';
 import { DokkuAPIProvider } from '../../shared/providers/DokkuProvider/implementations/DokkuAPIProvider';
 import { ConfigModule } from '@nestjs/config';
+import { NewCommitController } from './useCases/newCommit/NewCommitController';
+import { NewCommitUseCase } from './useCases/newCommit/NewCommitUseCase';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [NewRepositoryController],
+  controllers: [NewCommitController],
   providers: [
-    NewRepositoryUseCase,
+    NewCommitUseCase,
     { provide: IDokkuProvider, useClass: DokkuAPIProvider },
   ],
 })
